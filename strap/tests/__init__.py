83e3ba9a359275b1aa520e65b1b54a181cbe4d20
Whit Morriss <whit@surveymonkey.com>
2011-05-02 17:51:35 -0700

    working reqfile -> executable bundle bootstrap script + tests

--- strap/tests/__init__.py
+++ strap/tests/__init__.py
@@ -1,1 +1,26 @@
-#
+from contextlib import contextmanager
+import os
+
+
+@contextmanager
+def pushd(dir):
+    '''A context manager (Python 2.5+ only) for stepping into a 
+    directory and automatically coming back to the previous one. 
+    The original directory is returned. Usage is like this::
+    
+        from __future__ import with_statement
+        # the above line is only needed for Python 2.5
+        
+        from paver.easy import *
+        
+        @task
+        def my_task():
+            with pushd('new/directory') as old_dir:
+                ...do stuff...
+    '''
+    old_dir = os.getcwd()
+    os.chdir(dir)
+    try:
+        yield old_dir
+    finally:
+        os.chdir(old_dir)
--- strap/tests/example_extratext.py
+++ strap/tests/example_extratext.py
@@ -1,1 +1,29 @@
 #
+def extend_parser(optparse_parser):
+    """
+    You can add or remove options from the parser here.
+    """
+    print "parser"
+
+
+def adjust_options(options, args):
+##         You can change options here, or change the args (if you accept
+##         different kinds of arguments, be sure you modify ``args`` so it is
+##         only ``[DEST_DIR]``).
+    pass
+
+    
+def after_install(options, home_dir):
+##         After everything is installed, this function is called.  This
+##         is probably the function you are most likely to use.  An
+##         example would be::
+    
+##             def after_install(options, home_dir):
+##                 subprocess.call([join(home_dir, 'bin', 'easy_install'),
+##                                  'MyPackage'])
+##                 subprocess.call([join(home_dir, 'bin', 'my-package-script'),
+##                                  'setup', home_dir])
+    
+##         This example immediately installs a package, and runs a setup
+##         script from that package.
+    pass
