909d122f74cf313d25b7f343fadad21e81935632
Whit Morriss <whit@surveymonkey.com>
2011-07-30 00:27:36 -0600

    add coverage to ignore, remove dependency on fabric

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
-import sys, os
+import os
+import sys
 
 version = '0.1'
 
 setup(name='Strap',
       version=version,
       description="A bootstrap creator",
-      long_description="""\
-""",
+      long_description=""" """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
       author='whit at surveymonkey.com',
@@ -18,7 +18,6 @@ setup(name='Strap',
       include_package_data=True,
       zip_safe=False,
       install_requires=["path.py",
-                        "Fabric",
                         "virtualenv",
                         "pip"],
       entry_points="""
